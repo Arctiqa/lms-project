2bba7fe89d7597d0a55acb6f806f7475e6f42b9d
Arctiqa <Sinitro@yandex.ru>
2024-04-24 23:16:13 +0800

    .

---
+++ lms/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название')
+    preview = models.ImageField(upload_to='course_previews', default='course_previews/default.jpg',
+                                verbose_name='превью')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='lesson_previews', default='lesson_previews/default.jpg',
+                                verbose_name='превью')
+    link = models.CharField(max_length=200, verbose_name='ссылка', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=20, verbose_name='телефон')
+    city = models.CharField(max_length=50, verbose_name='город', **NULLABLE)
+    avatar = models.ImageField(upload_to='avatars', default='avatars/default.jpg', verbose_name='аватар')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
