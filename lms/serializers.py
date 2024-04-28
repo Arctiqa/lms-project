1e484844601cbf04c6a625aef3e95748fb67c1f8
Arctiqa <Sinitro@yandex.ru>
2024-04-28 15:07:11 +0800

    filters

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,6 +1,8 @@
-from rest_framework import serializers, fields
+from rest_framework import serializers
 
 from lms.models import Course, Lesson
+# from users.models import Payment
+# from users.serializers import PaymentSerializer
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -12,10 +14,15 @@ class LessonSerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
+    # payment_info = serializers.SerializerMethodField()
+    #
+    # def get_payment_info(self, obj):
+    #     payments = Payment.objects.filter(paid_course=obj)
+    #     return PaymentSerializer(payments, many=True).data
 
     def get_lessons_count(self, instance):
         return instance.lesson_set.all().count()
 
     class Meta:
         model = Course
-        fields = ('id', 'name', 'lessons_count', 'lessons')
+        fields = ('id', 'name', 'lessons_count', 'lessons')  # payment_info
